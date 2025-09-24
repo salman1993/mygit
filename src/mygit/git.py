237709016eee8054b8e10f0d7ff3a936246b4668
Salman Mohammed <smohammed1993@gmail.com>
2025-09-24 16:15:05 -0400

    write_index, git add

--- src/mygit/git.py
+++ src/mygit/git.py
@@ -1,10 +1,14 @@
 import sys
+import os
 import argparse
 import zlib
 from pathlib import Path
 from dataclasses import dataclass
 import hashlib
-
+import struct
+import difflib
+import functools
+import operator
 
 @dataclass
 class Object:
@@ -16,6 +20,23 @@ class Object:
         """Canonical git object representation"""
         header = f"{self.type} {self.size}".encode("utf-8")
         return header + b"\0" + self.content
+
+
+@dataclass
+class IndexEntry:
+    ctime_s: int
+    ctime_ns: int
+    mtime_s: int
+    mtime_ns: int
+    dev: int
+    ino: int
+    mode: int  # split into 2 parts: object type, unix permissions
+    uid: int
+    gid: int
+    size: int
+    sha1: bytes
+    flags: int
+    path: str
 
 
 def compute_digest(obj: Object) -> str:
@@ -38,6 +59,19 @@ def init(repo_dir: str) -> None:
     print(f"Initialized empty Git repository in {gitdir}")
 
 
+def check_gitdir(func):
+    """Decorator to check if current directory is a git repository."""
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        if not Path(".git").exists():
+            print("fatal: not a git repository", file=sys.stderr)
+            sys.exit(1)
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
 def _obj_path(obj: str) -> Path:
     p = Path(".git").joinpath("objects", obj[:2], obj[2:])
     if not p.exists():
@@ -48,19 +82,18 @@ def _obj_path(obj: str) -> Path:
 def _zlib_compress(obj: Object, output_path: Path):
     data = obj.construct_data()
     compressed_data = zlib.compress(data)
-    with open(output_path, "wb") as f_out:
-        f_out.write(compressed_data)
+    output_path.write_bytes(compressed_data)
 
 
 def _zlib_decompress(input_path: Path) -> Object:
-    with open(input_path, "rb") as f_in:
-        compressed_data = f_in.read()
+    compressed_data = input_path.read_bytes()
     decompressed = zlib.decompress(compressed_data)
     header, body = decompressed.split(b"\0", 2)
     typ, size = header.decode().split(" ")
     return Object(type=typ, size=int(size), content=body)
 
 
+@check_gitdir
 def cat_file(t: bool, s: bool, p: bool, obj_hash: str):
     objpath = _obj_path(obj_hash)
     obj = _zlib_decompress(objpath)
@@ -75,9 +108,15 @@ def cat_file(t: bool, s: bool, p: bool, obj_hash: str):
         sys.exit(1)
 
 
+def _hash_object(typ: str, filepath: str) -> str:
+    content = Path(filepath).read_bytes()
+    obj = Object(type=typ, size=len(content), content=content)
+    return compute_digest(obj)
+
+
+@check_gitdir
 def hash_object(typ: str, filepath: str, should_write: bool):
-    with open(filepath, "rb") as f_in:
-        content = f_in.read()
+    content = Path(filepath).read_bytes()
     obj = Object(type=typ, size=len(content), content=content)
     digest = compute_digest(obj)
     if should_write:
@@ -86,6 +125,210 @@ def hash_object(typ: str, filepath: str, should_write: bool):
         output_path = dirpath.joinpath(digest[2:])
         _zlib_compress(obj, output_path=output_path)
         print(f"Saved zlib compressed file at {output_path}")
+
+
+def read_index() -> list[IndexEntry]:
+    try:
+        data = Path(".git/index").read_bytes()
+    except FileNotFoundError:
+        return []
+
+    # git index format: https://manpages.ubuntu.com/manpages/plucky/man5/gitformat-index.5.html
+    # The first 12 bytes are the header, the last 20 a SHA-1 hash of the index, and the bytes in between are index entries, each 62 bytes plus the length of the path and some padding.
+    header = data[:12]
+    entries_data = data[12:-20]
+    sha1_hash = data[-20:]
+
+    assert hashlib.sha1(header + entries_data).digest() == sha1_hash, (
+        "invalid index checksum"
+    )
+
+    signature, version, num_entries = struct.unpack("!4sLL", header)
+    assert signature == b"DIRC", f"invalid index signature {signature}"
+    assert version == 2, f"unknown index version {version}"
+
+    entries = []
+    i = 0
+    while i + 62 < len(entries_data):
+        endi = i + 62
+        fields = struct.unpack("!LLLLLLLLLL20sH", entries_data[i:endi])
+        path_end = entries_data.index(b"\x00", endi)
+        path = entries_data[endi:path_end]
+        entries.append(IndexEntry(*(list(fields) + [path.decode()])))
+
+        # 1-8 nul bytes as necessary to pad the entry to a multiple of eight bytes
+        #            while keeping the name NUL-terminated.
+        entry_len = ((62 + len(path) + 8) // 8) * 8
+        i += entry_len
+
+    assert len(entries) == num_entries
+    return entries
+
+
+def write_index(entries: list[IndexEntry]):
+    signature = b"DIRC"
+    version = 2
+    num_entries = len(entries)
+
+    # git index format: https://manpages.ubuntu.com/manpages/plucky/man5/gitformat-index.5.html
+    # The first 12 bytes are the header, the last 20 a SHA-1 hash of the index, and the bytes in between are index entries, each 62 bytes plus the length of the path and some padding.
+    header = signature + int.to_bytes(version) + int.to_bytes(num_entries)
+    packed_entries: list[bytes] = []
+    for e in entries:
+        packed = (
+            struct.pack(
+                "!LLLLLLLLLL20sH",
+                e.ctime_s,
+                e.ctime_ns,
+                e.mtime_s,
+                e.mtime_ns,
+                e.dev,
+                e.ino,
+                e.mode,
+                e.uid,
+                e.gid,
+                e.size,
+                e.sha1,
+                e.flags,
+            )
+            + e.path.encode()
+        )
+        length = ((len(packed) + 8) // 8) * 8
+        num_padding = length - len(packed)
+        packed += b"\x00" * num_padding
+        packed_entries.append(packed)
+
+    entries_data = b"".join(packed_entries)
+    sha1_hash = hashlib.sha1(header + entries_data).digest()
+
+    content = header + entries_data + sha1_hash
+    Path(".git/index").write_bytes(content)
+
+
+@check_gitdir
+def add(filepaths: list[str]):
+    current_entries = read_index()
+
+    # add paths to entries that are not touched by `filepaths`
+    entries = [e for e in current_entries if e.path not in filepaths]
+
+    for path in filepaths:
+        if not Path(path).exists():
+           print(f"File {path} does not exist")
+           sys.exit(1)
+
+        # add or overwrite the file to the index
+        sha1 = _hash_object(typ='blob', filepath=path)
+        st = os.stat(path)
+        flags = len(path.encode())
+        assert flags < (1 << 12)
+        entry = IndexEntry(
+                int(st.st_ctime), 0, int(st.st_mtime), 0, st.st_dev,
+                st.st_ino, st.st_mode, st.st_uid, st.st_gid, st.st_size,
+                bytes.fromhex(sha1), flags, path)
+        entries.append(entry)
+
+    entries.sort(key=operator.attrgetter('path'))
+    write_index(entries)
+
+
+
+@check_gitdir
+def ls_files(stage: bool = False, debug: bool = False):
+    """Print list of files in index (including mode, SHA-1, and stage number
+    if 'stage' is True; extended metadata if 'debug' is True).
+    """
+    for entry in read_index():
+        stage_num = (entry.flags >> 12) & 3
+
+        if stage:
+            print(
+                "{:06o} {} {}\t{}".format(
+                    entry.mode, entry.sha1.hex(), stage_num, entry.path
+                )
+            )
+        else:
+            print(entry.path)
+
+        if debug:
+            print(f"  ctime: {entry.ctime_s}:{entry.ctime_ns}")
+            print(f"  mtime: {entry.mtime_s}:{entry.mtime_ns}")
+            print(f"  dev: {entry.dev} ino: {entry.ino}")
+            print(f"  uid: {entry.uid}      gid: {entry.gid}")
+            print(f"  size: {entry.size}      flags: {entry.flags}")
+
+
+def _cwd_filepaths() -> list[Path]:
+    paths: list[Path] = []
+    for p in Path(".").rglob("*"):
+        if ".git" not in p.parts:
+            paths.append(p)
+    return paths
+
+
+def _compare_cwd_files_to_index() -> tuple[list[Path], list[Path], list[Path]]:
+    """Returns the deleted, modified, untracked file paths. We don't show newly added files.
+
+    Untracked files: Files that exist in the working directory but are not in the index at all
+    Added files: Files that are in the index (staged) but weren't in the previous commit
+    """
+    index_entries = read_index()
+    index_paths = set([Path(e.path) for e in index_entries])
+    cwd_paths = set(_cwd_filepaths())
+
+    untracked = cwd_paths - index_paths
+    deleted = index_paths - cwd_paths
+    overlapping = index_paths & cwd_paths
+
+    modified: list[Path] = []
+    index_path_hash = {e.path: e.sha1 for e in index_entries}
+    for p in overlapping:
+        curr_hash = _hash_object(typ="blob", filepath=str(p))
+        prev_hash = index_path_hash[str(p)].hex()
+        if curr_hash != prev_hash:
+            modified.append(p)
+
+    return sorted(list(deleted)), sorted(list(modified)), sorted(list(untracked))
+
+
+@check_gitdir
+def status():
+    deleted, modified, untracked = _compare_cwd_files_to_index()
+
+    for p in deleted:
+        print(f"deleted:   {str(p)}")
+    for p in modified:
+        print(f"modified:  {str(p)}")
+
+    if untracked:
+        print("\nUntracked files:")
+        for p in untracked:
+            print(str(p))
+
+
+@check_gitdir
+def diff():
+    deleted, modified, _ = _compare_cwd_files_to_index()
+    entries = {e.path: e for e in read_index()}
+    for p in deleted + modified:
+        curr_content = p.read_text().splitlines()
+        index_obj_hash = entries[str(p)].sha1.hex()
+        objpath = _obj_path(index_obj_hash)
+        obj = _zlib_decompress(objpath)
+        index_content = obj.content.decode().splitlines()
+
+        diff_lines = difflib.unified_diff(
+            index_content,
+            curr_content,
+            fromfile=f"{p} - index",
+            tofile=f"{p} - current",
+        )
+
+        for l in diff_lines:
+            print(l)
+
+        print("-" * 60)
+        print()
 
 
 def main() -> None:
@@ -101,7 +344,7 @@ def main() -> None:
 
     # init
     sp = subparsers.add_parser("init", help="Initialize a new repo")
-    sp.add_argument("directory")
+    sp.add_argument("directory", nargs="?", default=".")
 
     # cat-file
     sp = subparsers.add_parser(
@@ -122,6 +365,32 @@ def main() -> None:
     sp.add_argument("-w", "--write", action="store_true")
     sp.add_argument("file", help="Path to the object file")
 
+    # ls-files
+    sp = subparsers.add_parser(
+        "ls-files",
+        help="Show information about files in the index and the working tree",
+    )
+    sp.add_argument("-s", "--stage", action="store_true")
+    sp.add_argument("-d", "--debug", action="store_true")
+
+    # status
+    sp = subparsers.add_parser(
+        "status",
+        help="Show working tree status",
+    )
+
+    # diff
+    sp = subparsers.add_parser(
+        "diff",
+        help="Show changes between commits, commit and working tree, etc",
+    )
+
+    # add
+    sp = subparsers.add_parser(
+        "add", help="Add file contents to the index"
+    )
+    sp.add_argument("filepaths", nargs='+',  help="File paths to add")
+
     args = parser.parse_args()
 
     match args.command:
@@ -133,6 +402,14 @@ def main() -> None:
             return hash_object(
                 typ=args.type, filepath=args.file, should_write=args.write
             )
+        case "ls-files":
+            ls_files(stage=args.stage, debug=args.debug)
+        case "status":
+            return status()
+        case "diff":
+            return diff()
+        case "add":
+            return add(args.filepaths)
         case _:
             print("Unknown command")
             sys.exit(1)
